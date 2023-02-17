e9df563f78b6b22d9cfa68a3ce25f3e31ee3e468
David Granados <73331579+David-Granados@users.noreply.github.com>
2023-02-17 19:18:55 +0100

    change atributes "ropa"

--- models/ropa.js
+++ models/ropa.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ropaSchema = new Schema({
-    nombre: String,
     tipo_prenda: String,
     talla: String,
-    color: String
+    color: String,
+    precio: String
 })
 
 //Creamos el modelo
